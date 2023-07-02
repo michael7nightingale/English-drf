6e3aa54b3af4c18915c8bb76abba31d9655f8d08
Michael Nightingale <suslanchikmopl@gmail.com>
2023-07-02 23:17:29 +0500

    settings for email

--- server/core/settings.py
+++ server/core/settings.py
@@ -6,7 +6,7 @@ from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv(".dev.env")
+load_dotenv("../.env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Server config
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 
 # Application definition
@@ -107,6 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+# Authentication model
+AUTH_USER_MODEL = "users.User"
 
 
 # Internationalization
@@ -227,3 +230,17 @@ CACHES = {
 }
 
 TIME_FORMAT = r"%Y-%m-%d %H:%M:%S.%f %z"
+
+# Email backend
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_FROM = os.getenv("EMAIL_FROM")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_USE_TLS = bool(os.getenv("EMAIL_USE_TLS", False))
+
+try:
+    PASSWORD_RESET_TIMEOUT = int(os.getenv("PASSWORD_RESET_TIMEOUT"))
+except ValueError:
+    PASSWORD_RESET_TIMEOUT = 14400
